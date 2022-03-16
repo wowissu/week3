bb2e65e8dbcff30e75fd9978c5c8be72aea5adad
wowissu <bbgbbg0123@gmail.com>
2022-03-16 22:59:29 +0800

    add MyButton for props demo

---
+++ src/components/MyButton.vue
@@ -0,0 +1,34 @@
+
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    // text: String as PropType<string>,
+
+    // 必填
+    // text: { required: true, type: String as PropType<string> },
+
+    // 多型態
+    // text: { required: true, type: [String, Number, Boolean] },
+
+    // 預設值 (有default )
+    // text: { default: 'button', type: String as PropType<string> }
+
+    // 驗證
+    text: {
+      type: String as PropType<string>,
+      validator(value: string) {
+        // The value must match one of these strings
+        return ['success', 'warning', 'danger'].includes(value)
+      }
+    }
+  }
+})
+</script>
+
+<template>
+  <button>
+    {{ text }}
+  </button>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue'
 import TheWelcome from '@/components/TheWelcome.vue'
+import MyButton from '@/components/MyButton.vue'
+
+export default defineComponent({
+  name: 'HelloWorld',
+  components: {
+    TheWelcome,
+    MyButton
+  }
+})
 </script>
 
 <template>
   <main>
+
+    <div>
+      <MyButton :text="'我的按鈕'"></MyButton>
+    </div>
+
     <TheWelcome />
   </main>
 </template>
